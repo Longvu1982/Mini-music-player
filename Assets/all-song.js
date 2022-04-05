f701d1864a5591bc728ae6346a4bc94cef347a81
Longvu1982 <tranngochiep200047@gmail.com>
2022-04-06 01:40:22 +0700

    first commit

---
+++ Assets/all-song.js
@@ -0,0 +1,58 @@
+const songs =
+    [
+        {
+            name: 'The search',
+            author: 'NF',
+            path: './Assets/songs/Song1 - The_search-NF.mp3',
+            image: './Assets/thumbnails/Song1.jpg',
+        },
+        {
+            name: 'Thủ Đô Cypher',
+            author: 'Rappers mashup',
+            path: './Assets/songs/Song2 - Thủ Đô Cypher  BeckStage X Bitis Hunter  RPT Orijinn LOW G RZMas RPT MCK.mp3',
+            image: './Assets/thumbnails/Song2.jpg',
+        },
+        {
+            name: 'Càng cua',
+            author: 'Low G',
+            path: './Assets/songs/Song3 - Càng Cua  Low G x Last Fire Crew  Nhà Hóa Học Đống Đa.mp3',
+            image: './Assets/thumbnails/Song3.jpg',
+        },
+        {
+            name: 'Anh thấy mình yếu đuối',
+            author: 'Tùa',
+            path: './Assets/songs/Song4 - CM1X  NẾU ANH THẤY LÒNG MÌNH YẾU ĐUỐI  TÙA  MUSIC VIDEO.mp3',
+            image: './Assets/thumbnails/Song4.jpg',
+        },
+        {
+            name: 'Khi em lớn',
+            author: 'Orange x Hoàng Dũng',
+            path: './Assets/songs/Song5 - Orange x Hoàng Dũng  Khi Em Lớn Official MV.mp3',
+            image: './Assets/thumbnails/Song5.jpg',
+        },
+        {
+            name: 'Rap Chậm Thôi',
+            author: 'RPT MCK',
+            path: './Assets/songs/Song6 - Rap Cham Thoi HNDCMM  RPT MCK x RPT Orijinn ft RZ Ma Official Lyric Video.mp3',
+            image: './Assets/thumbnails/Song6.jpg',
+        },
+        {
+            name: 'Mercy Acoustic',
+            author: 'Shawn Mendes',
+            path: './Assets/songs/Song7 - Shawn Mendes  Mercy Acoustic.mp3',
+            image: './Assets/thumbnails/Song7.jpg',
+        },
+        {
+            name: 'Monster',
+            author: 'Shawn Mendes x JB',
+            path: './Assets/songs/Song8 - Shawn Mendes Justin Bieber  Monster.mp3',
+            image: './Assets/thumbnails/Song8.jpg',
+        },
+        {
+            name: 'Lily',
+            author: 'Alan Walker',
+            path: './Assets/songs/Song9 - Vietsub  Lyrics Lily  Alan Walker K391  Emelie Hollow.mp3',
+            image: './Assets/thumbnails/Song9.jpg',
+        },
+    
+    ]
